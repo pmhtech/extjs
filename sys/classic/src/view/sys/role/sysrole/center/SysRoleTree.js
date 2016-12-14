a581a83b363f66c947da6f18c4e0ca9a1ed238d4
Muhyung Park <contact@pmhtech.net>
2016-12-14 15:56:09 +0900

    우측 TreeFilter 구현함.

--- sys/classic/src/view/sys/role/sysrole/center/SysRoleTree.js
+++ sys/classic/src/view/sys/role/sysrole/center/SysRoleTree.js
@@ -1,25 +1,50 @@
-
-Ext.define('SysApp.view.sys.role.sysrole.center.SysRoleTree',{
-    extend: 'Ext.tree.Panel',
-    alias: 'widget.sys-role-tree',
-    controller: 'sys-role-tree',
-    rootVisible: false,
-    store : Ext.create('Ext.data.TreeStore',{
-        root: {
-            MENU_NM: 'All',
-            text: 'ALL',
-            id : 'root',
-            expanded: true
-        }
-    }),
-    columns: [
-        {xtype: 'treecolumn',    text: '메뉴명',    dataIndex: 'MENU_NM',   flex: 1},
-        { text: '위젯명'     , dataIndex: 'WIDGET_NM', flex: 1},
-        {
-            text: '메뉴권한', dataIndex: 'MENU_AUTH', flex: 1,renderer : PmhTech.Format.comboRenderer,
-            editor: {
-                xtype: 'pmh-combo-code', store: SysCode['SYS_000002']
-            }
-        }
-    ]
+Ext.define('SysApp.view.sys.role.sysrole.center.SysRoleTree', {
+	extend: 'Ext.tree.Panel',
+	alias: 'widget.sys-role-tree',
+	controller: 'sys-role-tree',
+	rootVisible: false,
+	plugins: [{
+		ptype: 'pmh-treefilter'
+		, collapseOnClear: false
+		, allowParentFolders: true
+	}],
+	store: Ext.create('Ext.data.TreeStore', {
+		root: {
+			MENU_NM: 'All',
+			text: 'ALL',
+			id: 'root',
+			expanded: true
+		}
+	}),
+	dockedItems: [{
+		xtype: 'toolbar',
+		dock: 'top',
+		items: [{
+			xtype: 'textfield',
+			fieldLabel : '메뉴명',
+			name : 'MENU_NM',
+			listeners: {
+				change: 'onChangeFilter'
+			}
+		}, {
+			xtype: 'pmh-combo-code',
+			store: SysCode['SYS_000002'].copy(),
+			fieldLabel: '메뉴권한',
+			name : 'MENU_AUTH',
+			value : 'ALL',
+			listeners: {
+				change: 'onChangeFilter'
+			}
+		}]
+	}],
+	columns: [
+		{xtype: 'treecolumn', text: '메뉴명', dataIndex: 'MENU_NM', flex: 1},
+		{text: '위젯명', dataIndex: 'WIDGET_NM', flex: 1},
+		{
+			text: '메뉴권한', dataIndex: 'MENU_AUTH', flex: 1, renderer: PmhTech.Format.comboRenderer,
+			editor: {
+				xtype: 'pmh-combo-code', store: SysCode['SYS_000002']
+			}
+		}
+	]
 });
--- sys/classic/src/view/sys/role/sysrole/center/SysRoleTreeController.js
+++ sys/classic/src/view/sys/role/sysrole/center/SysRoleTreeController.js
@@ -1,5 +1,30 @@
 Ext.define('SysApp.view.sys.role.sysrole.center.SysRoleTreeController', {
     extend: 'Ext.app.ViewController',
-    alias: 'controller.sys-role-tree'
-    
+    alias: 'controller.sys-role-tree',
+
+    onChangeFilter : function(field){
+
+        var me = this.getView();
+        var MENU_AUTH = me.down('[name=MENU_AUTH]').getValue();
+        var MENU_NM = me.down('[name=MENU_NM]').getValue();
+
+
+
+        filterFunc = function(node){
+
+            var isEmptyMENU_NM = Ext.isEmpty(node.get('MENU_NM')) ? true : false;
+            var isEmptyMENU_AUTH = MENU_AUTH =='ALL' ? true: false;
+
+            var compareMENU_NM = isEmptyMENU_NM ? true : node.get('MENU_NM').search(MENU_NM)!=-1;
+            var compareMENU_AUTH = isEmptyMENU_AUTH ? true : node.get('MENU_AUTH') == MENU_AUTH;
+
+            return compareMENU_NM && compareMENU_AUTH;
+        };
+
+
+
+
+        this.getView().filterBy(filterFunc);
+
+    }
 });
