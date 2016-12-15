2aa4155118920f9d6b0c9ba1c31af4e64c75b05a
Muhyung Park <contact@pmhtech.net>
2016-12-16 03:36:10 +0900

    SysCode 리팩토링

--- sys/classic/src/view/sys/code/SysCode.js
+++ sys/classic/src/view/sys/code/SysCode.js
@@ -1,54 +1,63 @@
 Ext.define('SysApp.view.sys.code.SysCode', {
-    extend: 'SysApp.view.content.HBoxDefaultView',
-    alias: 'widget.sys-code',
-    requires : ['SysApp.view.sys.code.syscode.popup.SysCodePopup'],
-    controller: 'sys-code',
+	extend: 'SysApp.view.content.DefaultView',
+	alias: 'widget.sys-code',
+	layout: 'border',
+	requires: ['SysApp.view.sys.code.syscode.popup.SysCodePopup'],
+	controller: 'sys-code',
 
-    initComponent : function(){
-        var me = this;
+	initComponent: function () {
+		var me = this;
 
-        Ext.apply(me,{
-            leftItems: [{
-                title: '코드그룹목록 ',
-                xtype: 'sys-code-group-grid',
-                onGridSelect: 'onSelectGrid'
+		Ext.apply(me, {
+			items: [{
+				title: '코드그룹목록 ',
+				xtype: 'sys-code-group-grid',
+				region: 'west',
+				flex: 1,
+				onGridSelect: 'onSelectGrid'
+			}, {
+				xtype: 'container',
+				region: 'center',
+				flex: 1,
+				layout: {
+					type: 'vbox',
+					align: 'stretch'
+				},
+				items: [{
+					xtype: 'sys-code-grid',
+					title: '코드목록',
+					layout: 'fit',
+					height : 250,
+					onGridSelect: 'onSelectDetailGrid'
+				}, {
+					xtype: 'sys-code-tab',
+					flex: 1,
+					layout: 'fit',
+					frame: true,
+					tabBar: {
+						items: [{
+							xtype: 'tbfill'
+						}, {
+							xtype: 'pmh-button-add',
+							handler: 'onBtnAdd',
+							scope: me.getController()
+						}, {
+							xtype: 'pmh-button-reset',
+							handler: 'onBtnReset',
+							scope: me.getController()
+						}, {
+							xtype: 'pmh-button-save',
+							handler: 'onBtnSave',
+							scope: me.getController()
+						}]
+					}
+				}]
+			}]
 
-            }],
-            rightItems: [{
-                xtype: 'sys-code-grid',
-                title: '코드목록',
-                layout : 'fit',
-                flex: 1,
-                onGridSelect: 'onSelectDetailGrid'
-            },{
-                xtype: 'sys-code-tab',
-                frame : true,
-                tabBar :{
-                    items: [{
-                        xtype : 'tbfill'
-                    },{
-                        xtype: 'pmh-button-add',
-                        handler: 'onBtnAdd',
-                        scope: me.getController()
-                    },{
-                        xtype: 'pmh-button-reset',
-                        handler: 'onBtnReset',
-                        scope: me.getController()
-                    },{
-                        xtype: 'pmh-button-save',
-                        handler: 'onBtnSave',
-                        scope: me.getController()
-                    }]
-                },
-                layout : 'fit',
-                flex: 3
-            }]
+		});
 
-        });
-
-        me.callParent(arguments);
+		me.callParent(arguments);
 
 
-
-    }
+	}
 });
